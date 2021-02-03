534cfc49b02fcd1a17b501971064b2bc836ab547
blegloannec <blg@gmx.com>
2021-02-03 16:14:38 +0100

    ctr

---
+++ 3/3.19.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+
+import base64
+from Cryptodome.Cipher import AES
+from Cryptodome.Random import get_random_bytes
+
+BS = 16
+
+
+data_file = 'data/19.txt'  # NB: this also works well for 20.txt
+# we cheat a bit by setting the input to lowercase and extracting the charset
+_DATA = [base64.b64decode(line.strip()).lower()
+         for line in open(data_file, 'rb').readlines()]
+ALPHA = set(b''.join(_DATA))
+zero = bytes(BS//2)
+_key = get_random_bytes(BS)
+CIPH = [bytearray(AES.new(_key, AES.MODE_CTR, nonce=zero).encrypt(msg))
+        for msg in _DATA]
+
+
+# because the nonce is the same, all the "columns" of the ciphertexts are
+# xored with the same byte, allowing frequency attacks (provided we have
+# enough texts)
+# here we do a more basic & brutal charset attack
+DECIPH = [bytearray(ciph) for ciph in CIPH]
+nb_col = max(len(ciph) for ciph in CIPH)
+for b in range(nb_col):
+    col = [(i, ciph[b]) for i, ciph in enumerate(CIPH) if b < len(ciph)]
+    score_max = 0
+    for x in range(256):
+        # trying xor byte b = x
+        if all(c^x in ALPHA for _, c in col):
+            # this gives an acceptable charset
+            # let us maximize the number of [ a-z]
+            score = sum(int(c^x == ord(' ') or
+                            ord('a') <= c^x <= ord('z'))
+                        for _, c in col)
+            if score > score_max:
+                score_max = score
+                x_max = x
+    for i, _ in col:
+        DECIPH[i][b] ^= x_max
+
+
+# check
+for deciph, msg in zip(DECIPH, _DATA):
+    msg = msg.decode()
+    deciph = deciph.decode()
+    ham = sum(int(a!=b) for a,b in zip(deciph, msg))
+    if ham == 0:
+        print(' ', deciph)
+    else:
+        print('|', deciph, '|', ham, 'error(s)')
+        print('|', msg, '|')
