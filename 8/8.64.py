77b1424dd23db97036d4172ae2a9e76c67a8cd9c
blegloannec <blg@gmx.com>
2021-09-07 16:37:36 +0200

    clean.

--- 8/8.64.py
+++ 8/8.64.py
@@ -9,7 +9,6 @@ import os.path, pickle
 ## Parameters
 BS = gcm.BS      # 16 bytes
 bs = 8*BS        # 128 bits
-msk = (1<<bs)-1
 
 n  = 9           # n-1 = 8 rows forced to 0
 HS = 2           # 2 bytes = 16 bits hash size
@@ -49,20 +48,18 @@ def oracle(ciph_mac):
 # They can be represented by the following 128x128 matrices.
 
 # squaring operator matrix
-Cs = cmatrix(bs, bs, [poly2.pmodmul(1<<i, 1<<i) for i in range(bs)])
-Rs = cr_swap(Cs)
+CMsqr = cmatrix(bs, bs, [poly2.pmodmul(1<<i, 1<<i) for i in range(bs)])
+RMsqr = cr_swap(CMsqr)
 # powers of 2 (iterated squaring) operators matrices
-Cp = [None, Cs]
-Rp = [None, Rs]
-for _ in range(n+2):
-    Cp.append(rcc_mul(Rs, Cp[-1]))
-    Rp.append(cr_swap(Cp[-1]))
+CMpow2 = [None, CMsqr]
+for _ in range(n):
+    CMpow2.append(rcc_mul(RMsqr, CMpow2[-1]))
 
 # constant mult. operator matrix
-def gen_Rc(c):
-    Cc = cmatrix(bs, bs, [poly2.pmodmul(c, 1<<i) for i in range(bs)])
-    Rc = cr_swap(Cc)
-    return Rc
+def gen_RMcst(c):
+    CMcst = cmatrix(bs, bs, [poly2.pmodmul(c, 1<<i) for i in range(bs)])
+    RMcst = cr_swap(CMcst)
+    return RMcst
 
 # Consider the authentication (without add. data) of a ciphertext [ck c(k-1) .. c1]
 #   mac = s + c0 h + ∑ ci h^i  for s the auth. mask and c0 the size block
@@ -77,13 +74,20 @@ def gen_Rc(c):
 # let Ad = ∑ Mc(dj) Ms^j
 # truncated MAC collision:
 #   the first hs bits of mac'-mac = 0  <=>  the first hs bits of Ad h = 0
-def gen_Ad(D):
-    # D = [d1, ..., dn]
+
+def enum_d(d):
+    # d in {0,1}^128n -> [d1 .. dn] with di in {0,1}^128
+    msk = (1<<bs)-1
+    for i in range(n):
+        di = (d>>(i*bs)) & msk
+        yield (i+1, di)
+
+def gen_A(d):
     A = cmatrix(bs, bs)
-    for i,di in enumerate(D):
+    for i,di in enum_d(d):
         if di:
-            Rc = gen_Rc(di)
-            A = c_add(A, rcc_mul(Rc, Cp[i+1]))
+            RMdi = gen_RMcst(di)
+            A = c_add(A, rcc_mul(RMdi, CMpow2[i]))
     A = cr_swap(A)
     return A
 
@@ -93,16 +97,14 @@ def gen_Ad(D):
 # for (ei)_i the canonical base of GF(2)^(128n).
 # NB: This is completely independent from the parameters,
 #     this only depends on the GF(2^128) field we use.
+
 def gen_canonical_A(print_progress=True):
     CanonicalA = []
-    for i in range(n):
-        if print_progress:
-            print(i, end=', ', flush=True)
-        for b in range(bs):
-            D = [0]*n
-            D[i] |= 1<<b
-            A = gen_Ad(D)
-            CanonicalA.append(A)
+    for i in range(n*bs):
+        if print_progress and i%bs == 0:
+            print(i//bs, end=', ', flush=True)
+        A = gen_A(1<<i)
+        CanonicalA.append(A)
     return CanonicalA
 
 # If we can find some d such that the first z lines of Ad are forced
@@ -121,6 +123,7 @@ def gen_canonical_A(print_progress=True):
 # very small ker (while we need k >> hs-z).
 # Choosing z = n-1, we are sure that k ≥ 128 and we will
 # always have more than enough candidates.
+
 def gen_T(CanonicalA, zerows=n-1):
     # zerows: number of rows of Ad to stuff in T (to force to 0)
     #         n-1 by default
@@ -150,8 +153,7 @@ def randvec(V):
 # alter message according to given d
 def alter_msg(msg, d):
     C = [gcm.bytes_to_poly(msg[i:i+BS]) for i in reversed(range(0, len(msg), BS))]
-    for i in range(1, n+1):
-        di = (d>>((i-1)*bs)) & msk
+    for i,di in enum_d(d):
         C[(1<<i)-2] ^= di
     out = b''.join(gcm.poly_to_bytes(c) for c in reversed(C))
     return out
@@ -159,20 +161,15 @@ def alter_msg(msg, d):
 
 ## Sanity check
 def sanity_check():
-    u = secrets.randbelow(1<<(n*bs))
+    d = secrets.randbelow(1<<(n*bs))
     ciph1,mac1 = gcm.AES_GCM_encrypt(_key, _nonce, _msg)
-    ciph2 = alter_msg(ciph1, u)
+    ciph2 = alter_msg(ciph1, d)
     mac2 = gcm._aes_gcm_mac(_key, _nonce, ciph2)
     mac1 = gcm.bytes_to_poly(mac1)
     mac2 = gcm.bytes_to_poly(mac2)
-    d = mac1^mac2
-    C1 = [gcm.bytes_to_poly(ciph1[i:i+BS]) for i in reversed(range(0, len(ciph1), BS))]
-    C2 = [gcm.bytes_to_poly(ciph2[i:i+BS]) for i in reversed(range(0, len(ciph2), BS))]
-    D1 = [C1[(1<<i)-2] for i in range(1, n+1)]
-    D2 = [C2[(1<<i)-2] for i in range(1, n+1)]
-    D = [a^b for a,b in zip(D1,D2)]
-    A = gen_Ad(D)
-    assert rv_mul(A, _h) == d
+    dmac = mac1^mac2
+    A = gen_A(d)
+    assert rv_mul(A, _h) == dmac
 
 
 ## == Attacks == ##
@@ -193,8 +190,7 @@ def basic_attack(ciph, mac):
                 print(f'Looking for collision... ok ({cnt}).')
                 break
             cnt += 1
-        D = [(d>>(i*bs))&msk for i in range(n)]
-        A = gen_Ad(D)
+        A = gen_A(d)
         K = r_extend(K, A.M[n-1:hs])  # non-zero lines corresponding to the truncated hash
         if K.r >= bs-1:
             # reducing K to a free set of vectors
@@ -236,8 +232,7 @@ def accelerated_attack(ciph, mac):
                 print(f'Looking for collision... ok ({cnt}).')
                 break
             cnt += 1
-        D = [(d>>(i*bs))&msk for i in range(n)]
-        A = gen_Ad(D)
+        A = gen_A(d)
         K = r_extend(K, A.M[zerows:hs])  # non-zero lines corresponding to the truncated hash
         print("Updating X...")
         X = r_nullspace(K)
--- 8/8.64_32bits.py
+++ 8/8.64_32bits.py
@@ -21,13 +21,10 @@ import os.path, pickle
 ## Parameters
 BS = gcm.BS      # 16 bytes
 bs = 8*BS        # 128 bits
-msk = (1<<bs)-1
 
 n  = 17          # n-1 = 16 rows forced to 0
 HS = 4           # 2 bytes = 32 bits hash size
 hs = 8*HS
-hmsk = (1<<hs)-1
-
 
 # pre-computed data file (re-computed when missing)
 fprecomp = f'64_data_{n}.pickle'
@@ -59,14 +56,15 @@ def oracle(ciph_mac):
 '''
 
 _poly_h2i = [_h]
-for _ in range(n+2):
+for _ in range(n):
     _poly_h2i.append(poly2.pmodmul(_poly_h2i[-1], _poly_h2i[-1]))
 
-def fast_oracle(D):
-    d = 0
-    for i,di in enumerate(D):
-        d ^= poly2.pmodmul(di, _poly_h2i[i+1])
-    return d&hmsk == 0
+def fast_oracle(d):
+    dmac = 0
+    for i,di in enum_d(d):
+        dmac ^= poly2.pmodmul(di, _poly_h2i[i])
+    hmsk = (1<<hs)-1
+    return dmac & hmsk == 0
 
 
 ## == Operators matrices == ##
@@ -75,20 +73,18 @@ def fast_oracle(D):
 # They can be represented by the following 128x128 matrices.
 
 # squaring operator matrix
-Cs = cmatrix(bs, bs, [poly2.pmodmul(1<<i, 1<<i) for i in range(bs)])
-Rs = cr_swap(Cs)
+CMsqr = cmatrix(bs, bs, [poly2.pmodmul(1<<i, 1<<i) for i in range(bs)])
+RMsqr = cr_swap(CMsqr)
 # powers of 2 (iterated squaring) operators matrices
-Cp = [None, Cs]
-Rp = [None, Rs]
-for _ in range(n+2):
-    Cp.append(rcc_mul(Rs, Cp[-1]))
-    Rp.append(cr_swap(Cp[-1]))
+CMpow2 = [None, CMsqr]
+for _ in range(n):
+    CMpow2.append(rcc_mul(RMsqr, CMpow2[-1]))
 
 # constant mult. operator matrix
-def gen_Rc(c):
-    Cc = cmatrix(bs, bs, [poly2.pmodmul(c, 1<<i) for i in range(bs)])
-    Rc = cr_swap(Cc)
-    return Rc
+def gen_RMcst(c):
+    CMcst = cmatrix(bs, bs, [poly2.pmodmul(c, 1<<i) for i in range(bs)])
+    RMcst = cr_swap(CMcst)
+    return RMcst
 
 # Consider the authentication (without add. data) of a ciphertext [ck c(k-1) .. c1]
 #   mac = s + c0 h + ∑ ci h^i  for s the auth. mask and c0 the size block
@@ -103,13 +99,20 @@ def gen_Rc(c):
 # let Ad = ∑ Mc(dj) Ms^j
 # truncated MAC collision:
 #   the first hs bits of mac'-mac = 0  <=>  the first hs bits of Ad h = 0
-def gen_Ad(D):
-    # D = [d1, ..., dn]
+
+def enum_d(d):
+    # d in {0,1}^128n -> [d1 .. dn] with di in {0,1}^128
+    msk = (1<<bs)-1
+    for i in range(n):
+        di = (d>>(i*bs)) & msk
+        yield (i+1, di)
+
+def gen_A(d):
     A = cmatrix(bs, bs)
-    for i,di in enumerate(D):
+    for i,di in enum_d(d):
         if di:
-            Rc = gen_Rc(di)
-            A = c_add(A, rcc_mul(Rc, Cp[i+1]))
+            RMdi = gen_RMcst(di)
+            A = c_add(A, rcc_mul(RMdi, CMpow2[i]))
     A = cr_swap(A)
     return A
 
@@ -119,16 +122,14 @@ def gen_Ad(D):
 # for (ei)_i the canonical base of GF(2)^(128n).
 # NB: This is completely independent from the parameters,
 #     this only depends on the GF(2^128) field we use.
+
 def gen_canonical_A(print_progress=True):
     CanonicalA = []
-    for i in range(n):
-        if print_progress:
-            print(i, end=', ', flush=True)
-        for b in range(bs):
-            D = [0]*n
-            D[i] |= 1<<b
-            A = gen_Ad(D)
-            CanonicalA.append(A)
+    for i in range(n*bs):
+        if print_progress and i%bs == 0:
+            print(i//bs, end=', ', flush=True)
+        A = gen_A(1<<i)
+        CanonicalA.append(A)
     return CanonicalA
 
 # If we can find some d such that the first z lines of Ad are forced
@@ -147,6 +148,7 @@ def gen_canonical_A(print_progress=True):
 # very small ker (while we need k >> hs-z).
 # Choosing z = n-1, we are sure that k ≥ 128 and we will
 # always have more than enough candidates.
+
 def gen_T(CanonicalA, zerows=n-1):
     # zerows: number of rows of Ad to stuff in T (to force to 0)
     #         n-1 by default
@@ -177,8 +179,7 @@ def randvec(V):
 # alter message according to given d
 def alter_msg(msg, d):
     C = [gcm.bytes_to_poly(msg[i:i+BS]) for i in reversed(range(0, len(msg), BS))]
-    for i in range(1, n+1):
-        di = (d>>((i-1)*bs)) & msk
+    for i,di in enum_d(d):
         C[(1<<i)-2] ^= di
     out = b''.join(gcm.poly_to_bytes(c) for c in reversed(C))
     return out
@@ -198,12 +199,11 @@ def basic_attack():
         while True:
             print(f'Looking for collision... {cnt}' , end='\r', flush=True)
             d = randvec(NT)
-            D = [(d>>(i*bs))&msk for i in range(n)]
-            if fast_oracle(D):
+            if fast_oracle(d):
                 print(f'Looking for collision... ok ({cnt}).')
                 break
             cnt += 1
-        A = gen_Ad(D)
+        A = gen_A(d)
         K = r_extend(K, A.M[n-1:hs])  # non-zero lines corresponding to the truncated hash
         if K.r >= bs-1:
             # reducing K to a free set of vectors
@@ -240,12 +240,11 @@ def accelerated_attack():
         while True:
             print(f'Looking for collision... {cnt}' , end='\r', flush=True)
             d = randvec(NewNT)
-            D = [(d>>(i*bs))&msk for i in range(n)]
-            if fast_oracle(D):
+            if fast_oracle(d):
                 print(f'Looking for collision... ok ({cnt}).')
                 break
             cnt += 1
-        A = gen_Ad(D)
+        A = gen_A(d)
         K = r_extend(K, A.M[zerows:hs])  # non-zero lines corresponding to the truncated hash
         print("Updating X...")
         X = r_nullspace(K)
